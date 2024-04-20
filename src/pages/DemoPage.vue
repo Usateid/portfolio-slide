17ea498bf9b6d10f6417652e7968862ebc125b1a
Davide Cirone <davcir.dc@gmail.com>
2024-04-20 20:11:08 +0200

    feat: add h2 element in DemoPage

--- src/pages/DemoPage.vue
+++ src/pages/DemoPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-4 mx-auto prose md:px-6 prose-indigo sm:rounded-md">
     <h2>Demo Page</h2>
-
+    <h2>help help help</h2>
     <p>
       <router-link to="/">Back to home</router-link>
     </p>
