7f7710b3319fabbe2fc4620d49f0a52f862dd36e
Davide Cirone <davcir.dc@gmail.com>
2024-04-20 19:50:27 +0200

    a

--- tests/e2e/basic.spec.ts
+++ tests/e2e/basic.spec.ts
@@ -4,7 +4,6 @@ const defaultTitle = 'Vite + Vue + TypeScript + Tailwind Starter Template'
 
 test('homepage details', async ({ page }) => {
   await page.goto('/')
-
   await expect(page).toHaveTitle(defaultTitle)
   await expect(page.locator('h2').first()).toHaveText('Hello World Component')
 })
