95dd8117b15881adf155178ac5b71e39aa9cbf5c
Davide Cirone <davcir.dc@gmail.com>
2024-04-20 20:26:28 +0200

    feat: lasciare in staging

--- src/pages/DemoPage.vue
+++ src/pages/DemoPage.vue
@@ -2,6 +2,7 @@
   <div class="p-4 mx-auto prose md:px-6 prose-indigo sm:rounded-md">
     <h2>Demo Page</h2>
     <p>mod main</p>
+    <p>new update</p>
     <p>
       <router-link to="/">Back to home</router-link>
     </p>
